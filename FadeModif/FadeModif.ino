c8adce02364484f36604574a657503ee891fb7be
lucas e mercado <lucasemercado21@gmail.com>
2017-09-18 18:20:15 -0300

    change code fade.
    done #1

--- FadeModif/FadeModif.ino
+++ FadeModif/FadeModif.ino
@@ -8,9 +8,10 @@
  */
 
 int array_pin[] = {3,6,9,10,11,5};
-int brightness = 0;    // how bright the LED is
-int brightness2= 255; 
-int fadeAmount = 5;    // how many points to fade the LED by
+int brightness  = 0;    // how bright the LED is
+int brightness2 = 255; 
+int fadeAmount = 5;// how many points to fade the LED by
+int fadeAmount2 = 5;
 int cant_pin = 6;
 
 // the setup routine runs once when you press reset:
@@ -24,19 +25,26 @@ void setup() {
 void loop() {
   // set the brightness of pin 9:
   for(int i=0 ; i < cant_pin ; i++){
-    analogWrite(array_pin[i],brightness);
+    if(i<3){
+      analogWrite(array_pin[i],brightness); 
+    }
+    else{
+      analogWrite(array_pin[i],brightness2);
+    }
+    
   }
+  
   // change the brightness for next time through the loop:
   brightness += fadeAmount;
-  brighness2 -= fadeAmount;
+  brightness2 -= fadeAmount2;
 
   // reverse the direction of the fading at the ends of the fade:
   if (brightness == 0 || brightness == 255) {
     fadeAmount = -fadeAmount ;
   }
 
-  if(brightness2 == 0){
-    brighness2 = 255;
+ if(brightness2 == 0 || brightness2 == 255){
+    fadeAmount2 = -fadeAmount2 ;
   }
   
   // wait for 30 milliseconds to see the dimming effect
